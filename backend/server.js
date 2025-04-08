64b0e0e071d792a09a4e72eca84aba59a32d112d
Kishore2268 <kishore@anivarti.in>
2025-04-08 12:59:41 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -58,14 +58,11 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
   app.options('*', cors()); // handle preflight
-
 
 // Routes
 app.use('/api/auth', authRoutes);
